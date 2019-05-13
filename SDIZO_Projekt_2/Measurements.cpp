ddd7d8261d3e5b79c3594c2521420fba2849ff51
Piotrek <kpiotrek3@gmail.com>
2019-05-13 18:26:08 +0200

    Uwzględnienie BellmanaForda w pomiarach

--- SDIZO_Projekt_2/Measurements.cpp
+++ SDIZO_Projekt_2/Measurements.cpp
@@ -7,7 +7,8 @@ using namespace std;
 
 enum ALGORITHM_TYPE {
 	PRIM,
-	DIJKSTRA
+	DIJKSTRA,
+	BELLMANFORD
 };
 
 
@@ -59,6 +60,16 @@ void RunMeasurement(std::ostream& stream1, std::ostream& stream2, int numberOfNo
 			adjListCounter->EndSingleMeasurement();
 
 			break;
+
+		case BELLMANFORD:
+			adjMatrixCounter->StartNextMeasurement();
+			am->RunBellmanFord(0);
+			adjMatrixCounter->EndSingleMeasurement();
+
+			adjListCounter->StartNextMeasurement();
+			al->RunBellmanFord(0);
+			adjListCounter->EndSingleMeasurement();
+			break;
 		}
 	}
 
@@ -70,51 +81,78 @@ void RunMeasurement(std::ostream& stream1, std::ostream& stream2, int numberOfNo
 }
 
 void RunAllMeasurements() {
-	int nodes_to_research[] = { 10, 100, 200};
+	int nodes_to_research[] = { 10, 100, 200 };
 	const int nodes_to_research_size = 3;
-	int densities_to_research[] = {25, 50, 75, 99};
+	int densities_to_research[] = { 25, 50, 75, 99 };
 	const int densities_to_research_size = 4;
 
-	ofstream dijkstra_list;
-	dijkstra_list.open("results_dijkstra_list.txt");
+	{
+		ofstream dijkstra_list;
+		dijkstra_list.open("results_dijkstra_list.txt");
 
-	ofstream dijkstra_matrix;
-	dijkstra_matrix.open("results_dijkstra_matrix.txt");
+		ofstream dijkstra_matrix;
+		dijkstra_matrix.open("results_dijkstra_matrix.txt");
 
-	for (int n = 0; n < nodes_to_research_size; n++) {
-		for (int d = 0; d < densities_to_research_size; d++) {
-			cout << "Dijkstra N: " << n << " D: " << d << endl;
+		for (int n = 0; n < nodes_to_research_size; n++) {
+			for (int d = 0; d < densities_to_research_size; d++) {
+				cout << "Dijkstra N: " << n << " D: " << d << endl;
 
-			if (dijkstra_list && dijkstra_matrix) {
-				RunMeasurement(dijkstra_list, dijkstra_matrix, nodes_to_research[n], densities_to_research[d], 100, DIJKSTRA);
-			}
-			else {
-				throw exception("Nie mozna uzyskac dostepu do pliku zapisu");
-			}
-		}
-	}
-
-	dijkstra_list.close();
-	dijkstra_matrix.close();
-
-	ofstream prim_list;
-	prim_list.open("results_prim_list.txt");
-
-	ofstream prim_matrix;
-	prim_matrix.open("results_prim_matrix.txt");
-
-	for (int n = 0; n < nodes_to_research_size; n++) {
-		for (int d = 0; d < densities_to_research_size; d++) {
-			cout << "Prim N: " << n << " D: " << d << endl;
-			if (prim_list && prim_matrix) {
-				RunMeasurement(prim_list, prim_matrix, 10, 50, 100, PRIM);
-			}
-			else {
-				throw exception("Nie mozna uzyskac dostepu do pliku zapisu");
+				if (dijkstra_list && dijkstra_matrix) {
+					RunMeasurement(dijkstra_list, dijkstra_matrix, nodes_to_research[n], densities_to_research[d], 100, DIJKSTRA);
+				}
+				else {
+					throw exception("Nie mozna uzyskac dostepu do pliku zapisu");
+				}
 			}
 		}
+
+		dijkstra_list.close();
+		dijkstra_matrix.close();
 	}
 
-	prim_list.close();
-	prim_matrix.close();
+	{
+		ofstream prim_list;
+		prim_list.open("results_prim_list.txt");
+
+		ofstream prim_matrix;
+		prim_matrix.open("results_prim_matrix.txt");
+
+		for (int n = 0; n < nodes_to_research_size; n++) {
+			for (int d = 0; d < densities_to_research_size; d++) {
+				cout << "Prim N: " << n << " D: " << d << endl;
+				if (prim_list && prim_matrix) {
+					RunMeasurement(prim_list, prim_matrix, 10, 50, 100, PRIM);
+				}
+				else {
+					throw exception("Nie mozna uzyskac dostepu do pliku zapisu");
+				}
+			}
+		}
+
+		prim_list.close();
+		prim_matrix.close();
+	}
+
+	{
+		ofstream bf_list;
+		bf_list.open("results_bf_list.txt");
+
+		ofstream bf_matrix;
+		bf_matrix.open("results_bf_matrix.txt");
+
+		for (int n = 0; n < nodes_to_research_size; n++) {
+			for (int d = 0; d < densities_to_research_size; d++) {
+				cout << "BF N: " << n << " D: " << d << endl;
+				if (bf_list && bf_matrix) {
+					RunMeasurement(bf_list, bf_matrix, 10, 50, 100, BELLMANFORD);
+				}
+				else {
+					throw exception("Nie mozna uzyskac dostepu do pliku zapisu");
+				}
+			}
+		}
+
+		bf_list.close();
+		bf_matrix.close();
+	}
 }
